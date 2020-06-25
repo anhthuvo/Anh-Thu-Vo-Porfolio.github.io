5281e2a09e8ca1085999c6433485e77ff513d8fe
AnhThuVo <newzon263@gmail.com>
2020-06-25 11:17:38 +0700

    fixed navbar hide when scrolling in large screen, separate js file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,43 @@
 $(document).ready(function(){
 
-// navbar down
+    // navbar show when click
     $(".nav__btn__down").click(function(){
-        $("nav").slideToggle(500);
+        $("nav").toggle(1000);
         $(this).hide();
         $(".nav__btn__up").show();
     });
 
     $(".nav__btn__up").click(function(){
-        $("nav").slideToggle(500);
+        $("nav").toggle(1000);
         $(this).hide();
         $(".nav__btn__down").show();
     });
- 
+
+
+
+  
+        
+
+    // click outside navbar, navbar hide
+    $("body").click(function(event){
+        console.log(event.target.className);
+        if ( $("nav").css("display") == 'block'){
+          if ( event.target.className.indexOf("nav-link") == -1
+                && event.target.className.indexOf("nav__btn__down") == -1
+                && event.target.className.indexOf("nav__btn__up") == -1 ){
+                   $("nav").toggle();
+                   $(".nav__btn__up").hide();
+                    $(".nav__btn__down").show();
+          }
+        }
+    });
+    
+    //   $(window).scroll(function() {
+    //     // navbar hide when scrolling in mobile size
+    //     if ( $("nav").css("display") == 'block' && 
+    //          $(window).width() <= 767.98) {
+    //         navtoggle();
+    //     }
+
+    //   });
 });
